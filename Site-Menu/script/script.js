4b3911dc5128fc4fe136da42f29d00884457aa78
Ismael <ismael2021ab@gmail.com>
2023-09-17 00:22:49 -0300

    Feat: updated

---
+++ Site-Menu/script/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+
+
+    const itens = document.getElementById('usr-Menu')
+    const clk = document.getElementById('usr-Account')
+
+    clk.addEventListener('click', showhide)
+
+    function showhide()
+    {
+        if (itens.style.display == 'none')
+        {
+            itens.style.display = 'grid'
+        }
+        else
+        {
+            itens.style.display = 'none'
+        }
+        
+    }
+
+
+
+})
+
+
